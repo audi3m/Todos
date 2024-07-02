a9670f1e98ab44cf4ac94fa7b77163289974d3f3
ray <audi3m@naver.com>
2024-07-03 00:55:51 +0900

    Add new Todo

--- Todos/Register/AddNewViewController.swift
+++ Todos/Register/AddNewViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SnapKit
+import RealmSwift
 
 final class AddNewViewController: BaseViewController {
     
@@ -28,6 +29,7 @@ final class AddNewViewController: BaseViewController {
         navigationItem.title = "새로운 할 일"
         let cancel = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(cancelButtonClicked))
         let add = UIBarButtonItem(title: "추가", style: .plain, target: self, action: #selector(addButtonClicked))
+        
         navigationItem.leftBarButtonItem = cancel
         navigationItem.rightBarButtonItem = add 
     }
@@ -47,8 +49,7 @@ final class AddNewViewController: BaseViewController {
         
         tableView.separatorStyle = .none
         tableView.backgroundColor = .systemBackground
-        
-        
+         
     }
     
     @objc func cancelButtonClicked() {
@@ -56,10 +57,20 @@ final class AddNewViewController: BaseViewController {
     }
     
     @objc func addButtonClicked() {
+        let titleCell = tableView.visibleCells.first as! TitleMemoTableViewCell
+        let text = titleCell.titleTextField.text!
+            
+        let realm = try! Realm()
+        let data = TodoModel(title: text, memo: "메모", dueDate: .now)
+        try! realm.write {
+            realm.add(data)
+            print("Realm Save Success")
+        }
+        
+        dismiss(animated: true)
         
     }
-    
-    
+     
 }
 
 extension AddNewViewController: UITableViewDelegate, UITableViewDataSource {
@@ -70,9 +81,6 @@ extension AddNewViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: TitleMemoTableViewCell.id, for: indexPath) as! TitleMemoTableViewCell
-            
-            
-            
             return cell
         } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: AddNewTableViewCell.id, for: indexPath) as! AddNewTableViewCell
@@ -85,8 +93,7 @@ extension AddNewViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.reloadRows(at: [indexPath], with: .automatic)
     }
-    
-    
+     
     private enum Attributes: String, CaseIterable {
         case titleAndMemo
         case dueDate = "마감일"
