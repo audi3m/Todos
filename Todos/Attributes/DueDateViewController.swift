5869cad168536ae494e620e704dfb26c2934f5e0
ray <audi3m@naver.com>
2024-07-06 16:31:23 +0900

    마감일 선택하기 & 삭제하기

--- Todos/Attributes/DueDateViewController.swift
+++ Todos/Attributes/DueDateViewController.swift
@@ -11,34 +11,53 @@ import SnapKit
 final class DueDateViewController: BaseViewController {
     
     let datePicker = UIDatePicker()
+    let noDueDateButton = UIButton()
     
-    var sendDate: ((Date) -> Void)?
+    var selectedDate: Date?
+    var sendDate: ((Date?) -> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "마감일"
-        
+        selectedDate = datePicker.date
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        sendDate?(datePicker.date) 
+        sendDate?(selectedDate)
     }
     
     
     override func setHierarchy() {
         view.addSubview(datePicker)
+        view.addSubview(noDueDateButton)
     }
     
     override func setLayout() {
         datePicker.snp.makeConstraints { make in
             make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
         }
+        
+        noDueDateButton.snp.makeConstraints { make in
+            make.top.equalTo(datePicker.snp.bottom).offset(30)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(30)
+            make.height.equalTo(50)
+        }
     }
     
     override func setUI() {
         datePicker.datePickerMode = .date
         datePicker.preferredDatePickerStyle = .inline
+        
+        noDueDateButton.setTitle("마감일 삭제", for: .normal)
+        noDueDateButton.backgroundColor = .systemRed
+        noDueDateButton.layer.cornerRadius = 10
+        noDueDateButton.addTarget(self, action: #selector(deleteDueDate), for: .touchUpInside)
+    }
+    
+    @objc private func deleteDueDate() {
+        selectedDate = nil
+        navigationController?.popViewController(animated: true)
     }
     
 }
